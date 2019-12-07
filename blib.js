3ab47de259eda8870235448a9740f3eb6c1730a4
Wtech82 <56077548+Wtech82@users.noreply.github.com>
2019-12-07 22:00:55 +0300

    Add files via upload

---
+++ blib.js
@@ -0,0 +1,44 @@
+function jQuery (selector, context = document){
+	this.elements = Array.from(context.querySelectorAll(selector));
+	return this
+}
+
+jQuery.prototype.each = function (fn){
+	this.elements.forEach((element, index) => fn.call(element, element, index));
+	return this;
+}
+
+jQuery.prototype.click = function(fn){
+	this.each(element => element.addEventListener('click', fn))
+	return this
+}
+
+jQuery.prototype.hide = function(){
+	this.each(element => element.style.display = 'none')
+  return this;
+}
+
+jQuery.prototype.show = function(){
+	this.each(element => element.style.display = '')
+  return this;
+}
+
+jQuery.prototype.remove = function(){
+	this.each(element => element.remove())
+  return this;
+}
+
+jQuery.prototype.class = function(name){
+	this.each(element => element.className = name)
+  return this;
+}
+
+jQuery.prototype.html = function(html) {
+	this.each(element => element.innerHTML = html);
+	return this;
+}
+
+jQuery.prototype.text = function(text) {
+	this.each(element => element.textContent = text);
+	return this;
+}
